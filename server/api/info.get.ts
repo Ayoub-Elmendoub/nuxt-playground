7b88f3f68f20277b63f04e1ae3b886063448a248
Ayoub-Elmendoub <a.elmendoub@gmail.com>
2023-05-10 16:24:47 +0100

    feat(server): info get endpoint

---
+++ server/api/info.get.ts
@@ -0,0 +1,12 @@
+import { generate } from "openapi-typescript-codegen";
+
+export default defineEventHandler(async (event) => {
+  const { params, api } = event.context;
+  // const code = await generate({
+  //   input: "data/swagger.yaml",
+  //   output: "./generated",
+  // });
+  return {
+    info: api.info,
+  };
+});
