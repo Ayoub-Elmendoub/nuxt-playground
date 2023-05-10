e7d85db08aa4fc74e7507e3a9e7d03d56b417f5b
Ayoub-Elmendoub <a.elmendoub@gmail.com>
2023-05-10 18:27:16 +0100

    generate zip file from generated

--- server/api/bundle.get.ts
+++ server/api/bundle.get.ts
@@ -1,9 +1,33 @@
 import { generate } from "openapi-typescript-codegen";
+import { createWriteStream } from "fs";
+
+import archiver from "archiver";
+
+var output = createWriteStream("target.zip");
+var archive = archiver("zip");
+
+output.on("close", function () {
+  console.log(archive.pointer() + " total bytes");
+  console.log(
+    "archiver has been finalized and the output file descriptor has closed."
+  );
+});
+
+archive.on("error", function (err) {
+  throw err;
+});
+
+archive.pipe(output);
+
+// append files from a sub-directory, putting its contents at the root of archive
+archive.directory("./generated", "sdk");
+
+archive.finalize();
 
 export default defineEventHandler(async (event) => {
   await generate({
     input: "data/openapi.yaml",
     output: "./generated",
   });
-  return 'OK';
+  return "OK";
 });
