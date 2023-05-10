b33f74355e7d3ff94a176ac734fa2a4954b95fdd
Ayoub-Elmendoub <a.elmendoub@gmail.com>
2023-05-10 21:55:37 +0100

    fix(server): comment generating zip code

--- server/api/bundle.get.ts
+++ server/api/bundle.get.ts
@@ -3,26 +3,26 @@ import { createWriteStream } from "fs";
 
 import archiver from "archiver";
 
-var output = createWriteStream("target.zip");
-var archive = archiver("zip");
+// var output = createWriteStream("target.zip");
+// var archive = archiver("zip");
 
-output.on("close", function () {
-  console.log(archive.pointer() + " total bytes");
-  console.log(
-    "archiver has been finalized and the output file descriptor has closed."
-  );
-});
+// output.on("close", function () {
+//   console.log(archive.pointer() + " total bytes");
+//   console.log(
+//     "archiver has been finalized and the output file descriptor has closed."
+//   );
+// });
 
-archive.on("error", function (err) {
-  throw err;
-});
+// archive.on("error", function (err) {
+//   throw err;
+// });
 
-archive.pipe(output);
+// archive.pipe(output);
 
-// append files from a sub-directory, putting its contents at the root of archive
-archive.directory("./generated", "sdk");
+// // append files from a sub-directory, putting its contents at the root of archive
+// archive.directory("./generated", "sdk");
 
-archive.finalize();
+// archive.finalize();
 
 export default defineEventHandler(async (event) => {
   await generate({
